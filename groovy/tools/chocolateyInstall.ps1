a46b18362d1c9af03395eac2ed3087ff4ae111c5
Litee <lipatkin@gmail.com>
2018-04-15 22:30:43 +0300

    Upgraded Groovy to 2.4.15

--- groovy/tools/chocolateyInstall.ps1
+++ groovy/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'groovy'
 $packageVersion = $env:chocolateyPackageVersion
-$checksum = 'F9C846E21D9220BAC464FFC00EAE8F4146F4C26777B61F62E3123DA95C6BC52A'
+$checksum = 'BD4CA37A4D1B3704526D56FC48C119A8F70D418093D8703724407D65250F4AED'
 $url = "https://dl.bintray.com/groovy/maven/apache-groovy-binary-$packageVersion.zip"
 $toolsDir = Get-ToolsLocation
 $groovy_home = Join-Path $toolsDir "$packageName-$packageVersion"
