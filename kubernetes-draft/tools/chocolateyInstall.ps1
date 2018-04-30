2ea1a6b57768e61249012fd33f8b77ce8e072f3f
Litee <lipatkin@gmail.com>
2018-04-30 13:55:59 +0300

    First version of 'draft' package

---
+++ kubernetes-draft/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'kubernetes-draft'
+$packageVersion = $env:chocolateyPackageVersion
+$url = "https://azuredraft.blob.core.windows.net/draft/draft-v$packageVersion-windows-amd64.zip"
+$checksum = '973BAAF6BA62DB9F6014FA728721554057531E5A4B447165D15AFBA17F24FA61'
+
+$toolsDir   = Get-ToolsLocation
+$draft_home = Join-Path $toolsDir "$packageName-$packageVersion"
+
+Install-ChocolateyZipPackage `
+    -PackageName $packageName `
+    -UnzipLocation $draft_home `
+    -Url $url `
+    -Checksum $checksum `
+    -ChecksumType 'sha256'
+
+Install-ChocolateyPath `
+    -PathToInstall "$draft_home\windows-amd64" `
+    -PathType 'Machine'
