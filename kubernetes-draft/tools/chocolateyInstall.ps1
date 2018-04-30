48b94facfc7d7846dfe3f425f53244a062f273f7
Litee <400234+Litee@users.noreply.github.com>
2018-04-30 14:01:03 +0300

    Simplified deployment scripts of Draft

--- kubernetes-draft/tools/chocolateyInstall.ps1
+++ kubernetes-draft/tools/chocolateyInstall.ps1
@@ -3,16 +3,11 @@ $packageVersion = $env:chocolateyPackageVersion
 $url = "https://azuredraft.blob.core.windows.net/draft/draft-v$packageVersion-windows-amd64.zip"
 $checksum = '973BAAF6BA62DB9F6014FA728721554057531E5A4B447165D15AFBA17F24FA61'
 
-$toolsDir   = Get-ToolsLocation
-$draft_home = Join-Path $toolsDir "$packageName-$packageVersion"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage `
     -PackageName $packageName `
-    -UnzipLocation $draft_home `
+    -UnzipLocation $toolsDir `
     -Url $url `
     -Checksum $checksum `
     -ChecksumType 'sha256'
-
-Install-ChocolateyPath `
-    -PathToInstall "$draft_home\windows-amd64" `
-    -PathType 'Machine'
