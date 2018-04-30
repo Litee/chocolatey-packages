fac430945cacdde1d5519c35f2012d7256fbed83
Litee <400234+Litee@users.noreply.github.com>
2018-04-30 13:55:59 +0300

    First version of 'draft' package

---
+++ kubernetes-draft/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'kubernetes-draft'
+$packageVersion = $env:chocolateyPackageVersion
+$toolsDir = Get-ToolsLocation
+$draft_home = Join-Path $toolsDir "$packageName-$packageVersion"
+
+Remove-Item $draft_home -Recurse -Force
