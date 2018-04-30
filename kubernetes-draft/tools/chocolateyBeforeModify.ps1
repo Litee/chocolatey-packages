076c2395e34c9a8bb3f5b190f0e4581b9f8c97e9
Litee <lipatkin@gmail.com>
2018-04-30 14:01:03 +0300

    Simplified deployment scripts of Draft

--- kubernetes-draft/tools/chocolateyBeforeModify.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName = 'kubernetes-draft'
-$packageVersion = $env:chocolateyPackageVersion
-$toolsDir = Get-ToolsLocation
-$draft_home = Join-Path $toolsDir "$packageName-$packageVersion"
-
-Remove-Item $draft_home -Recurse -Force
