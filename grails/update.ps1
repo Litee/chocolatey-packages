2d02e4982478c64c7d40218e64bf246921ac8d57
Litee <lipatkin@gmail.com>
2018-05-09 14:07:53 +0300

    Added AU script for updating Grails

---
+++ grails/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/grails/grails-core/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*.url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*.checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url   = $download_page.links | Where-Object href -match '.zip$' | ForEach-Object href | Select-Object -First 1
+    $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+
+    @{
+        URL32   = 'https://github.com' + $url
+        Version = $version
+        # ChecksumType = 'sha256'
+    }
+}
+
+update
