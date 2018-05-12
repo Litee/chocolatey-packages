5350820aab05d4636da86376c01a926da083a437
Litee <400234+Litee@users.noreply.github.com>
2018-05-12 17:17:17 +0300

    Added Anki package

---
+++ anki/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$checksum = 'cca3236075acaa2e972a108d8d6d6437a375936b1e958da68891eff47ade433e'
+$url = 'https://apps.ankiweb.net/downloads/current/anki-2.0.51.exe'
+
+Install-ChocolateyPackage `
+	-PackageName 'anki' `
+	-FileType 'exe' `
+	-SilentArgs '/S'  `
+	-Url $url `
+	-Checksum $checksum `
+	-ChecksumType 'sha256'
