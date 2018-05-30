bf64d0af456888bb38178902dd1b760e02ee6d2a
Litee <lipatkin@gmail.com>
2018-05-30 21:32:24 +0300

    Added semi-automatic upgrade for Groovy, upgraded to v2.5.0

--- groovy/tools/chocolateyInstall.ps1
+++ groovy/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'groovy'
 $packageVersion = $env:chocolateyPackageVersion
-$checksum = 'BD4CA37A4D1B3704526D56FC48C119A8F70D418093D8703724407D65250F4AED'
-$url = "https://dl.bintray.com/groovy/maven/apache-groovy-binary-$packageVersion.zip"
+$checksum = '13903caed7da4875b2c805ddaa5ac4a3c877342f339700fab9a54b8c628eece3'
+$url = 'https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.0.zip'
 $toolsDir = Get-ToolsLocation
 $groovy_home = Join-Path $toolsDir "$packageName-$packageVersion"
 $groovy_bat = Join-Path $groovy_home 'bin/groovy.bat'
