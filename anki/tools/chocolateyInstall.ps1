7831063d3230e3b78da8bc737009e8aaee92702c
Litee <400234+Litee@users.noreply.github.com>
2018-06-03 11:04:20 +0300

    Upgraded anki to version 2.0.52

--- anki/tools/chocolateyInstall.ps1
+++ anki/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = 'cca3236075acaa2e972a108d8d6d6437a375936b1e958da68891eff47ade433e'
-$url = 'https://apps.ankiweb.net/downloads/current/anki-2.0.51.exe'
+﻿$checksum = '94e3b9840382d8a66ef1153c4b1435e83bdda2a22d7044b0f5b9131deb9ace4b'
+$url = 'https://apps.ankiweb.net/downloads/current/anki-2.0.52.exe'
 
 Install-ChocolateyPackage `
 	-PackageName 'anki' `
