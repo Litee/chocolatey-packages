79a41322fc03d19d9825970abeee036be5fe977b
Litee <400234+Litee@users.noreply.github.com>
2019-03-31 11:59:44 +0100

    Upgraded Anki to v2.1.11

--- anki/tools/chocolateyInstall.ps1
+++ anki/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = 'e7fa6e306644873ee3e8b826ebb5373f9bd879d6059fb7218446be243580dbc9'
-$url = 'https://apps.ankiweb.net/downloads/current/anki-2.1.8-windows.exe'
+﻿$checksum = '08988bdeadef801491d145780fe182f14e67e3a667cfb5f83145be40be0a2b43'
+$url = 'https://apps.ankiweb.net/downloads/current/anki-2.1.11-windows.exe'
 
 Install-ChocolateyPackage `
 	-PackageName 'anki' `
