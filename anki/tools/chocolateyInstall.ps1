367edd7f08eb92ed5e8916242d67b37ac00481a9
Ross Guarino <rssguar@gmail.com>
2019-06-14 17:51:19 -0700

    Update akni to 2.1.13

--- anki/tools/chocolateyInstall.ps1
+++ anki/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = '91383b6b24aee83ee86d721ec246a3763d5678c4a5daabcb1f7e1bdcde58b9a5'
-$url = 'https://apps.ankiweb.net/downloads/current/anki-2.1.12-windows.exe'
+﻿$checksum = 'f03ec672079c1a27a308519bb42d7439f4c3401dbb83947c5f15defbf3d6a19e'
+$url = 'https://apps.ankiweb.net/downloads/current/anki-2.1.13-windows.exe'
 
 Install-ChocolateyPackage `
 	-PackageName 'anki' `
