cef1f1196d00d498bd194faf6f25d956e8aa73f8
Litee <lipatkin@gmail.com>
2020-08-15 14:15:44 +0100

    Upgrade Groovy to v3.0.4

--- groovy/tools/chocolateyInstall.ps1
+++ groovy/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'groovy'
 $packageVersion = $env:chocolateyPackageVersion
-$checksum = 'f4b2211e7651ed4ec7728c96c1e1961795c4a9de7b0cad6d399df8e92001d682'
-$url = 'https://dl.bintray.com/groovy/maven/apache-groovy-binary-3.0.0.zip'
+$checksum = 'eba1dc30ed0ceeefe56eba6e79cf43c2ae164eea6e407fd7a79f6c434a667146'
+$url = 'https://dl.bintray.com/groovy/maven/apache-groovy-binary-3.0.4.zip'
 $toolsDir = Get-ToolsLocation
 $groovy_home = Join-Path $toolsDir "$packageName-$packageVersion"
 $groovy_bat = Join-Path $groovy_home 'bin/groovy.bat'
