aeef2a010c9ee85d64eba77cccd53a0c2ab4a90e
Litee <400234+Litee@users.noreply.github.com>
2020-08-15 14:15:11 +0100

    Upgraded Anki to v2/1/28

--- anki/update.ps1
+++ anki/update.ps1
@@ -12,11 +12,11 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri 'https://apps.ankiweb.net/'
 
-    $url = $download_page.links | Where-Object href -match 'current/anki-2.1.\d+-windows.exe' | Select-Object -First 1 -expand href
+    $url = $download_page.links | Where-Object href -match 'https://github.com/ankitects/anki/releases/download/\d+\.\d+\.\d+/anki-\d+\.\d+\.\d+-windows.exe' | Select-Object -First 1 -expand href
     $version = ([regex]::Match($url, '(\d+\.\d+\.\d+)-windows.exe')).Groups[1].Value
 
     @{
-        URL32   = 'https://apps.ankiweb.net' + $url
+        URL32   = $url
         Version = $version
         # ChecksumType = 'sha256'
     }
