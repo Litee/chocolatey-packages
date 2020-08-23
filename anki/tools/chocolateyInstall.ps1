4a0e9ba75a2dacc76f87f71c2e154e2dd99c78fa
Litee <lipatkin@gmail.com>
2020-08-24 00:42:43 +0100

    Upgraded Anki to v2.1.30

--- anki/tools/chocolateyInstall.ps1
+++ anki/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = '184467a2124e8309daef939d2612433390818f931f1d5761897bffebc00f0e46'
-$url = 'https://github.com/ankitects/anki/releases/download/2.1.28/anki-2.1.28-windows.exe'
+﻿$checksum = '36690689bb4869956f801d126c47133a43c328c3b0adee2f8bba45d489e9405b'
+$url = 'https://github.com/ankitects/anki/releases/download/2.1.30/anki-2.1.30-windows.exe'
 
 Install-ChocolateyPackage `
 	-PackageName 'anki' `
